21829c1d642e846822bc68b08cbf5d52241ab54a
ezekiel180624 <duongphucesl@gmail.com>
2024-11-20 09:15:50 +0700

    pac art

---
+++ Tkinter-Designer-master/database/data.py
@@ -0,0 +1,52 @@
+import mysql.connector
+
+db=mysql.connector.connect(host='localhost', user='root', password='Haimot@10')
+# Tạo con trỏ để thực thi câu lệnh SQL
+cursor = db.cursor()
+# Tạo cơ sở dữ liệu (nếu chưa có)
+cursor.execute("CREATE DATABASE IF NOT EXISTS pac_art")
+# Chọn cơ sở dữ liệu
+db.database = "pac_art"
+
+# Tạo bảng
+
+# user bable
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+''')
+
+# image table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS image (
+        id_image INT AUTO_INCREMENT PRIMARY KEY,
+        upload_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        name_image VARCHAR(255) NOT NULL,
+        image_path VARCHAR(255) NOT NULL,  -- Thêm trường này để lưu đường dẫn ảnh
+        format_image VARCHAR(50),           -- Thêm trường này để lưu định dạng ảnh (tùy chọn)
+        user_id INT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+    )
+''')
+
+# edited_image table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS edited_image (
+        edited_id_image INT AUTO_INCREMENT PRIMARY KEY,
+        name_image VARCHAR(255) NOT NULL,
+        image_path VARCHAR(255) NOT NULL,   -- Thêm trường này để lưu đường dẫn ảnh đã chỉnh sửa
+        format_image VARCHAR(50),            -- Thêm trường này để lưu định dạng ảnh (tùy chọn)
+        user_id INT NOT NULL,
+        edit_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+    )
+''')
+
+# Đóng con trỏ và kết nối
+cursor.close()
+db.close()
