47aa63538c6372bdab019f9d2ac0693533f74a52
kakashi-10 <166940268+kakashi-10@users.noreply.github.com>
2024-12-04 09:53:42 +0700

    pac art

---
+++ Tkinter-Designer-master/Build/globals.py
@@ -0,0 +1,10 @@
+# globals.py
+selected_image_path = None
+selected_folder_images = None
+
+class UserSession:
+    def __init__(self):
+        self.user_id = None
+
+# Khởi tạo đối tượng UserSession
+user_session = UserSession()
